e5d0c5202306d0d3facc0b52db9d943ad1864311
May Jawdat <may.jawdat@gmail.com>
2017-03-25 14:04:20 -0700

    Update gems, add sessions files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'auth/developer', :as => 'developer_auth'
+  match 'auth/:provider/callback' => 'session#create', :via => [:get, :post]
+
+  get 'session/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
